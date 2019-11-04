c2eb7a74696c0b893a6531a481181e4d20b7531b
jy392 <jy392@cornell.edu>
2019-11-04 12:47:17 -0500

    reset

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -6,13 +6,13 @@ import MeetupPreview from "./preview-templates/MeetupPreview";
 import FooterPreview from "./preview-templates/FooterPreview";
 import NavbarPreview from "./preview-templates/NavbarPreview";
 import PastMeetupsPagePreview from "./preview-templates/PastMeetupsPagePreview";
-import TeamPagePreview from "./preview-templates/TeamPagePreview";
+/*import TeamPagePreview from "./preview-templates/TeamPagePreview";
 import ContactPagePreview from "./preview-templates/ContactPagePreview";
 import NewPreview from "./preview-templates/NewPreview";
 import PastNewsPagePreview from "./preview-templates/PastNewsPagePreview";
 import EventPreview from "./preview-templates/EventPreview";
 import PastEventsPagePreview from "./preview-templates/PastEventsPagePreview";
-
+*/
 
 CMS.registerPreviewTemplate("meetups", MeetupPreview);
 CMS.registerPreviewTemplate("footer", FooterPreview);
@@ -20,9 +20,10 @@ CMS.registerPreviewTemplate("navbar", NavbarPreview);
 CMS.registerPreviewTemplate("about", AboutPagePreview);
 CMS.registerPreviewTemplate("home", HomePagePreview);
 CMS.registerPreviewTemplate("pastMeetups", PastMeetupsPagePreview);
-CMS.registerPreviewTemplate("news", NewPreview);
+/*CMS.registerPreviewTemplate("news", NewPreview);
 CMS.registerPreviewTemplate("pastNews", PastNewsPagePreview);
 CMS.registerPreviewTemplate("events", EventPreview);
 CMS.registerPreviewTemplate("pastEvents", PastEventsPagePreview);
 CMS.registerPreviewTemplate("team", TeamPagePreview);
 CMS.registerPreviewTemplate("contact", ContactPagePreview);
+*/
